b39343d00d01f61fec202b8d996e0277ddb0dc59
Igor Fernandes <ifc@cin.ufpe.br>
2019-12-01 20:52:33 -0300

    [API] Corrigido Logins da DB Seed

--- api/config/db/db_seeds.sql
+++ api/config/db/db_seeds.sql
@@ -10,36 +10,36 @@ INSERT INTO "user" Values('seiyazinho','88888888','Seiya de Pegasus', 'seiyazinh
 
 --Times
 
-INSERT INTO "team" VALUES ('OSS','Os Super Sayajins','Gokuzinho');
-INSERT INTO "team" VALUES ('CDS','Cavaleiros do Saara','Gintokizinho');
-INSERT INTO "team" VALUES ('CDZ','Cavaleiros do Zodiaco','Seiyazinho');
-INSERT INTO "team" VALUES ('FT','Fairy Tail','Natsuzinho');
-INSERT INTO "team" VALUES ('UA','Universidade de Herois','Midoriyazinho');
-INSERT INTO "team" VALUES ('SDS','Shinigamis do Sertão','Ichigozinho');
+INSERT INTO "team" VALUES ('OSS','Os Super Sayajins','gokuzinho');
+INSERT INTO "team" VALUES ('CDS','Cavaleiros do Saara','gintokizinho');
+INSERT INTO "team" VALUES ('CDZ','Cavaleiros do Zodiaco','seiyazinho');
+INSERT INTO "team" VALUES ('FT','Fairy Tail','natsuzinho');
+INSERT INTO "team" VALUES ('UA','Universidade de Herois','midoriyazinho');
+INSERT INTO "team" VALUES ('SDS','Shinigamis do Sertão','ichigozinho');
 
 --Torneios
 
-INSERT INTO "tournament" VALUES (1,'Melhor Torneio de YuGiOh','Melhor torneio de YuGiOh da sua rua, vai ser muito top vey','10/10/2019','11/10/2019','Gokuzinho','UA');
-INSERT INTO "tournament" VALUES (2,'Melhor Torneio de Robblox','O torneio que as crianças vão amar, um jogo que ninguem gosta, só tiram onda por que é um lixo','05/11/2019','07/11/2019','Gintokizinho','UA');
-INSERT INTO "tournament" VALUES (3,'Torneio Galático','Torneio de batalha entre deuses e grandes guerreiros de todas as galáxias','11/22/2019','11/26/2019','Gokuzinho','CDZ');
-INSERT INTO "tournament" VALUES (4,'Exame Hunter','Torneio para saber quais serão os próximos seres qualificados para possui a licença Hunter','05/11/2019','10/11/2019','Gonzinho','UA');
-INSERT INTO "tournament" VALUES (5,'Torneio de Counter Strike','Torneio que reune os melhores times do cenário brasileiro de cs-go, com premiação de incriveis 3 reais','12/17/2019','12/22/2019','Natsuzinho');
-INSERT INTO "tournament" VALUES (6,'Torneio de Espadas','Torneio que reune todos os espadachins dos mundos, da sociedade das almas, para saber quem é o mais forte','07/02/2020','03/13/2020','Ichigozinho');
+INSERT INTO "tournament" VALUES (1,'Melhor Torneio de YuGiOh','Melhor torneio de YuGiOh da sua rua, vai ser muito top vey','10/10/2019','11/10/2019','gokuzinho','UA');
+INSERT INTO "tournament" VALUES (2,'Melhor Torneio de Robblox','O torneio que as crianças vão amar, um jogo que ninguem gosta, só tiram onda por que é um lixo','05/11/2019','07/11/2019','gintokizinho','UA');
+INSERT INTO "tournament" VALUES (3,'Torneio Galático','Torneio de batalha entre deuses e grandes guerreiros de todas as galáxias','11/22/2019','11/26/2019','gokuzinho','CDZ');
+INSERT INTO "tournament" VALUES (4,'Exame Hunter','Torneio para saber quais serão os próximos seres qualificados para possui a licença Hunter','05/11/2019','10/11/2019','gonzinho','UA');
+INSERT INTO "tournament" VALUES (5,'Torneio de Counter Strike','Torneio que reune os melhores times do cenário brasileiro de cs-go, com premiação de incriveis 3 reais','12/17/2019','12/22/2019','natsuzinho');
+INSERT INTO "tournament" VALUES (6,'Torneio de Espadas','Torneio que reune todos os espadachins dos mundos, da sociedade das almas, para saber quem é o mais forte','07/02/2020','03/13/2020','ichigozinho');
 
 
 
 --Telefones
 
-INSERT INTO "phone" VALUES ('+558192024396','Gokuzinho');
-INSERT INTO "phone" VALUES ('+558179964184','Luffyzinho');
-INSERT INTO "phone" VALUES ('+558111441468','Ichigozinho');
-INSERT INTO "phone" VALUES ('+558172421724','Natsuzinho');
-INSERT INTO "phone" VALUES ('+558164802709','Gonzinho');
-INSERT INTO "phone" VALUES ('+558130807150','Gintokizinho');
-INSERT INTO "phone" VALUES ('+558123849320','Midoriyazinho');
-INSERT INTO "phone" VALUES ('+558100422296','Seiyazinho');
-INSERT INTO "phone" VALUES ('+558184587668','Gonzinho');
-INSERT INTO "phone" VALUES ('+558184587669','Gonzinho');
+INSERT INTO "phone" VALUES ('+558192024396','gokuzinho');
+INSERT INTO "phone" VALUES ('+558179964184','luffyzinho');
+INSERT INTO "phone" VALUES ('+558111441468','ichigozinho');
+INSERT INTO "phone" VALUES ('+558172421724','natsuzinho');
+INSERT INTO "phone" VALUES ('+558164802709','gonzinho');
+INSERT INTO "phone" VALUES ('+558130807150','gintokizinho');
+INSERT INTO "phone" VALUES ('+558123849320','midoriyazinho');
+INSERT INTO "phone" VALUES ('+558100422296','seiyazinho');
+INSERT INTO "phone" VALUES ('+558184587668','gonzinho');
+INSERT INTO "phone" VALUES ('+558184587669','gonzinho');
 
 --Partidas
 
@@ -98,84 +98,84 @@ INSERT INTO "play" VALUES('SDS',5 ,4);
 
 --Membros do time
 
-INSERT INTO "team_member" VALUES('Midoriyazinho', 'UA');
-INSERT INTO "team_member" VALUES('Natsuzinho', 'FT');
-INSERT INTO "team_member" VALUES('Gintokizinho', 'CDS');
-INSERT INTO "team_member" VALUES('Seiyazinho', 'CDZ');
-INSERT INTO "team_member" VALUES('Ichigozinho', 'SDS');
-INSERT INTO "team_member" VALUES('Gonzinho', 'OSS');
-INSERT INTO "team_member" VALUES('Luffyzinho', 'SDS');
-INSERT INTO "team_member" VALUES('Gokuzinho', 'CDZ');
+INSERT INTO "team_member" VALUES('midoriyazinho', 'UA');
+INSERT INTO "team_member" VALUES('natsuzinho', 'FT');
+INSERT INTO "team_member" VALUES('gintokizinho', 'CDS');
+INSERT INTO "team_member" VALUES('seiyazinho', 'CDZ');
+INSERT INTO "team_member" VALUES('ichigozinho', 'SDS');
+INSERT INTO "team_member" VALUES('gonzinho', 'OSS');
+INSERT INTO "team_member" VALUES('luffyzinho', 'SDS');
+INSERT INTO "team_member" VALUES('gokuzinho', 'CDZ');
 
 
 --Integrantes
 
-INSERT INTO "integrate" VALUES('Midoriyazinho', 'UA', 1);
-INSERT INTO "integrate" VALUES('Natsuzinho', 'FT', 1);
-INSERT INTO "integrate" VALUES('Gintokizinho', 'CDS', 1);
-INSERT INTO "integrate" VALUES('Seiyazinho', 'CDZ', 1);
-INSERT INTO "integrate" VALUES('Ichigozinho', 'SDS', 1);
+INSERT INTO "integrate" VALUES('midoriyazinho', 'UA', 1);
+INSERT INTO "integrate" VALUES('natsuzinho', 'FT', 1);
+INSERT INTO "integrate" VALUES('gintokizinho', 'CDS', 1);
+INSERT INTO "integrate" VALUES('seiyazinho', 'CDZ', 1);
+INSERT INTO "integrate" VALUES('ichigozinho', 'SDS', 1);
 
-INSERT INTO "integrate" VALUES('Natsuzinho', 'UA', 2);
-INSERT INTO "integrate" VALUES('Midoriyazinho', 'UA', 2);
-INSERT INTO "integrate" VALUES('Gokuzinho', 'CDZ', 2);
-INSERT INTO "integrate" VALUES('Ichigozinho', 'CDZ', 2);
-INSERT INTO "integrate" VALUES('Seiyazinho', 'OSS', 2);
-INSERT INTO "integrate" VALUES('Gonzinho', 'OSS', 2);
-INSERT INTO "integrate" VALUES('Gintokizinho', 'SDS', 2);
-INSERT INTO "integrate" VALUES('Luffyzinho', 'SDS', 2);
+INSERT INTO "integrate" VALUES('natsuzinho', 'UA', 2);
+INSERT INTO "integrate" VALUES('midoriyazinho', 'UA', 2);
+INSERT INTO "integrate" VALUES('gokuzinho', 'CDZ', 2);
+INSERT INTO "integrate" VALUES('ichigozinho', 'CDZ', 2);
+INSERT INTO "integrate" VALUES('seiyazinho', 'OSS', 2);
+INSERT INTO "integrate" VALUES('gonzinho', 'OSS', 2);
+INSERT INTO "integrate" VALUES('gintokizinho', 'SDS', 2);
+INSERT INTO "integrate" VALUES('luffyzinho', 'SDS', 2);
 
-INSERT INTO "integrate" VALUES('Luffyzinho', 'SDS', 4);
-INSERT INTO "integrate" VALUES('Midoriyazinho', 'UA', 4);
-INSERT INTO "integrate" VALUES('Gonzinho', 'FT', 4);
-INSERT INTO "integrate" VALUES('Ichigozinho','OSS', 4);
-INSERT INTO "integrate" VALUES('Gokuzinho', 'CDZ', 4);
+INSERT INTO "integrate" VALUES('luffyzinho', 'SDS', 4);
+INSERT INTO "integrate" VALUES('midoriyazinho', 'UA', 4);
+INSERT INTO "integrate" VALUES('gonzinho', 'FT', 4);
+INSERT INTO "integrate" VALUES('ichigozinho','OSS', 4);
+INSERT INTO "integrate" VALUES('gokuzinho', 'CDZ', 4);
 
 --Follows
 
-INSERT INTO "follow" VALUES('Natsuzinho', 'Gokuzinho');
-INSERT INTO "follow" VALUES('Gonzinho', 'Gokuzinho');
-INSERT INTO "follow" VALUES('Seiyazinho', 'Gokuzinho');
-INSERT INTO "follow" VALUES('Gonzinho', 'Ichigozinho');
-INSERT INTO "follow" VALUES('Gokuzinho', 'Seiyazinho');
-INSERT INTO "follow" VALUES('Gokuzinho', 'Midoriyazinho');
-INSERT INTO "follow" VALUES('Luffyzinho', 'Midoriyazinho');
-INSERT INTO "follow" VALUES('Luffyzinho', 'Natsuzinho');
-INSERT INTO "follow" VALUES('Gintokizinho', 'Natsuzinho');
-INSERT INTO "follow" VALUES('Ichigozinho', 'Seiyazinho');
-INSERT INTO "follow" VALUES('Midoriyazinho', 'Luffyzinho');
+INSERT INTO "follow" VALUES('natsuzinho', 'gokuzinho');
+INSERT INTO "follow" VALUES('gonzinho', 'gokuzinho');
+INSERT INTO "follow" VALUES('seiyazinho', 'gokuzinho');
+INSERT INTO "follow" VALUES('gonzinho', 'ichigozinho');
+INSERT INTO "follow" VALUES('gokuzinho', 'seiyazinho');
+INSERT INTO "follow" VALUES('gokuzinho', 'midoriyazinho');
+INSERT INTO "follow" VALUES('luffyzinho', 'midoriyazinho');
+INSERT INTO "follow" VALUES('luffyzinho', 'natsuzinho');
+INSERT INTO "follow" VALUES('gintokizinho', 'natsuzinho');
+INSERT INTO "follow" VALUES('ichigozinho', 'seiyazinho');
+INSERT INTO "follow" VALUES('midoriyazinho', 'luffyzinho');
 
 
 --Apostador
-INSERT INTO "punter" VALUES('Gintokizinho', 600.70);
-INSERT INTO "punter" VALUES('Ichigozinho', 902.32);
+INSERT INTO "punter" VALUES('gintokizinho', 600.70);
+INSERT INTO "punter" VALUES('ichigozinho', 902.32);
 
 --Aposta do MVP
 
-INSERT INTO "mvp_bet" VALUES('Gintokizinho','Seiyazinho', 'CDZ', 1);
-INSERT INTO "mvp_bet" VALUES('Ichigozinho','Midoriyazinho', 'UA', 1);
-INSERT INTO "mvp_bet" VALUES('Gintokizinho','Natsuzinho', 'UA', 2);
-INSERT INTO "mvp_bet" VALUES('Ichigozinho','Luffyzinho', 'SDS', 2);
-INSERT INTO "mvp_bet" VALUES('Gintokizinho','Luffyzinho', 'SDS', 4);
-INSERT INTO "mvp_bet" VALUES('Gintokizinho','Gokuzinho', 'CDZ', 4);
+INSERT INTO "mvp_bet" VALUES('gintokizinho','seiyazinho', 'CDZ', 1);
+INSERT INTO "mvp_bet" VALUES('ichigozinho','midoriyazinho', 'UA', 1);
+INSERT INTO "mvp_bet" VALUES('gintokizinho','natsuzinho', 'UA', 2);
+INSERT INTO "mvp_bet" VALUES('ichigozinho','luffyzinho', 'SDS', 2);
+INSERT INTO "mvp_bet" VALUES('gintokizinho','luffyzinho', 'SDS', 4);
+INSERT INTO "mvp_bet" VALUES('gintokizinho','gokuzinho', 'CDZ', 4);
 
 --Moderador
-INSERT INTO "moderator" VALUES('Gokuzinho');
-INSERT INTO "moderator" VALUES('Luffyzinho');
-INSERT INTO "moderator" VALUES('Midoriyazinho');
+INSERT INTO "moderator" VALUES('gokuzinho');
+INSERT INTO "moderator" VALUES('luffyzinho');
+INSERT INTO "moderator" VALUES('midoriyazinho');
 
 --Moderação
 
-INSERT INTO "moderate" VALUES('Gokuzinho',1);
-INSERT INTO "moderate" VALUES('Luffyzinho',2);
-INSERT INTO "moderate" VALUES('Midoriyazinho',4);
+INSERT INTO "moderate" VALUES('gokuzinho',1);
+INSERT INTO "moderate" VALUES('luffyzinho',2);
+INSERT INTO "moderate" VALUES('midoriyazinho',4);
 
 --Participantes
-INSERT INTO "participant" VALUES('Gokuzinho','Macaco Rabudo');
-INSERT INTO "participant" VALUES('Luffyzinho','Borrachudo');
-INSERT INTO "participant" VALUES('Ichigozinho','Espadachim Vagante');
-INSERT INTO "participant" VALUES('Natsuzinho','Maluco do Fogo');
-INSERT INTO "participant" VALUES('Gonzinho','Demonio Ingenuo');
-INSERT INTO "participant" VALUES('Seiyazinho','Cavaleiro de Pegasus');
-INSERT INTO "participant" VALUES('Gintokizinho','Prata');
-INSERT INTO "participant" VALUES('Midoriyazinho','Deku');
+INSERT INTO "participant" VALUES('gokuzinho','Macaco Rabudo');
+INSERT INTO "participant" VALUES('luffyzinho','Borrachudo');
+INSERT INTO "participant" VALUES('ichigozinho','Espadachim Vagante');
+INSERT INTO "participant" VALUES('natsuzinho','Maluco do Fogo');
+INSERT INTO "participant" VALUES('gonzinho','Demonio Ingenuo');
+INSERT INTO "participant" VALUES('seiyazinho','Cavaleiro de Pegasus');
+INSERT INTO "participant" VALUES('gintokizinho','Prata');
+INSERT INTO "participant" VALUES('midoriyazinho','Deku');
